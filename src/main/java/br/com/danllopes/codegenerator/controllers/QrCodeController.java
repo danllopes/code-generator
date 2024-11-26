a4443173ed152f9feae5a9d96c0ae31c6243e0f8
Daniel Lopes <danielanderson.lopes0@gmail.com>
2024-11-26 14:17:55 -0300

    ✨feat: Adiciona classe QrCodeController

---
+++ src/main/java/br/com/danllopes/codegenerator/controllers/QrCodeController.java
@@ -0,0 +1,11 @@
+package br.com.danllopes.codegenerator.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/qr")
+public class QrCodeController {
+
+
+}
