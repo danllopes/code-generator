c4fb93afebfca766cfc47d1ec9b3b92d0aa39bee
Daniel Lopes <danielanderson.lopes0@gmail.com>
2024-11-26 14:23:07 -0300

    ✨feat: Adiciona metodo POST responsavel por gerar o Qr Code

--- src/main/java/br/com/danllopes/codegenerator/controllers/QrCodeController.java
+++ src/main/java/br/com/danllopes/codegenerator/controllers/QrCodeController.java
@@ -1,8 +1,15 @@
 package br.com.danllopes.codegenerator.controllers;
 
 import br.com.danllopes.codegenerator.service.QrCodeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+import java.util.Map;
 
 @RestController
 @RequestMapping("v1/qr")
@@ -12,5 +19,25 @@ public class QrCodeController {
 
     public QrCodeController(QrCodeService service) {
         this.service = service;
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<?> generateQr(@RequestParam("qrString") String qrString) {
+        if (qrString == null || qrString.isEmpty()) {
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", "The field cannot be empty!"));
+        }
+
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            service.generateQrCode(qrString, outputStream);
+
+            String encodedQrCode  = "data:image/png;base64," +
+                    Base64.getEncoder().encodeToString(outputStream.toByteArray());
+
+            return ResponseEntity.ok(Map.of("qrCodeImage", encodedQrCode));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(Map.of("error", "Error generating QR Code!"));
+        }
     }
 }
