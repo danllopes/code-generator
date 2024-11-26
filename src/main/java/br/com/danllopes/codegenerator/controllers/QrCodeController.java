f3eb1886d40c19a861848ac8e2a9eabc5b0f58a8
Daniel Lopes <danielanderson.lopes0@gmail.com>
2024-11-26 14:19:15 -0300

    ✨feat: Injeta dependencia QrCodeService na classe QrCodeController

--- src/main/java/br/com/danllopes/codegenerator/controllers/QrCodeController.java
+++ src/main/java/br/com/danllopes/codegenerator/controllers/QrCodeController.java
@@ -1,5 +1,6 @@
 package br.com.danllopes.codegenerator.controllers;
 
+import br.com.danllopes.codegenerator.service.QrCodeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/qr")
 public class QrCodeController {
 
+    private final QrCodeService serivice;
 
+    public QrCodeController(QrCodeService service) {
+        this.serivice = service;
+    }
 }
